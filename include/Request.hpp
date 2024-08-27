929276942e1684d1836f716fa0b411fb95dabf90
Alisa Tonkopiy <atonkopi@paul-f4Ar4s3.clusters.42paris.fr>
2024-08-27 15:20:17 +0200

    formatting change

--- include/Request.hpp
+++ include/Request.hpp
@@ -21,27 +21,28 @@ private:
 	std::map<std::string, std::string> headers;
 	std::map<std::string, std::string> queries;
 	std::string body;
-	//parsing
+	// parsing
 	void parseRequest();
 	void parseRequestLine(const std::string &line);
-    void setUri(const std::string &str);
-    void setMethod(const std::string &str);
-    void setVersion(const std::string &str);
-   	void setHeaders(std::istringstream &stream);
-    bool isValidHeader(const std::string &line, std::string &name, std::string &value);
-    void parseHeaderName(const std::string &str, std::string &name);
-    void parseHeaderValue(const std::string &str, std::string &value);
+	void setUri(const std::string &str);
+	void setMethod(const std::string &str);
+	void setVersion(const std::string &str);
+	void setHeaders(std::istringstream &stream);
+	bool isValidHeader(const std::string &line, std::string &name, std::string &value);
+	void parseHeaderName(const std::string &str, std::string &name);
+	void parseHeaderValue(const std::string &str, std::string &value);
 	bool parseBody(std::string &body);
+
 public:
 	Request(const std::string &buffer);
 	~Request();
-	//getters
+	// getters
 	std::string getBuffer() const;
 	std::string getMethod() const;
 	std::string getUri() const;
 	std::string getVersion() const;
 	std::map<std::string, std::string> getHeaders() const;
 	std::string getBody() const;
-	//debug
+	// debug
 	void printRequest(Request &req);
 };
