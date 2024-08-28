7003b4b99156a1c5b59810044f5f530f1a4acb3b
vvaudain <vvaudain@student.42.fr>
2024-08-28 15:25:28 +0200

    main branch update

--- src/main.cpp
+++ src/main.cpp
@@ -32,10 +32,10 @@ int main(int ac, char **av)
 	ports.push_back(8080);
 	ports.push_back(8081);
 
-	server.SetResponse("HTTP/1.1 200 OK\n");
-	server.SetResponse("Content-Type: text/html\r\n");
-	server.SetResponse("Content-Length: 13\n\n");
-	server.SetResponse("Hello World !\r\n\r\n");
+	server.setResponse("HTTP/1.1 200 OK\n");
+	server.setResponse("Content-Type: text/html\r\n");
+	server.setResponse("Content-Length: 13\n\n");
+	server.setResponse("Hello World !\r\n\r\n");
 	
 	server.startServer(ports);
 	// server.CloseServer();
