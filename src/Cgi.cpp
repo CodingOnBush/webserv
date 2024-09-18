77398e691554f8f528cce48298328ba5ae564b85
Alisa Tonkopiy <atonkopi@made-f0Br7s19.clusters.42paris.fr>
2024-09-18 15:00:18 +0200

    added free env logic, mem leaks fixed + removed comments from cgi script

--- src/Cgi.cpp
+++ src/Cgi.cpp
@@ -1,68 +1,33 @@
 #include "Cgi.hpp"
 
-// void printCgiParams(const std::map<std::string, std::string>& cgiParams) {
-//     for (const auto& param : cgiParams) {
-//         std::cout << param.first << ": " << param.second << std::endl;
-//     }
-// }
-
-// we need to merge
-// we need to be clear with what's inside env
-
-// ENV
-/*
-method type : GET, POST, DELETE
-body : body of the request
-// query string : query string of the request ?? maybe no time
-content length : length of the body
-upload location : location of the file to upload
-*/
-
-
-	// create char **env from the request with vars
-	// use excve (pass env) in a fork to run the script and be able to receive the exit status (error, success, or more)
-	// python script
-
-char **createEnv(Request &req, LocationBlock &location) {
-    char **env = new char*[6];
+char **createEnv(Request &req, LocationBlock &location)
+{
+    char **env = new char *[6];
     std::stringstream ss;
-
     ss << "CONTENT_LENGTH=" << req.getBody().size();
     env[0] = strdup(ss.str().c_str());
-
     ss.str("");
     ss << "CONTENT_TYPE=" << req.getHeaders()["Content-Type"];
     env[1] = strdup(ss.str().c_str());
-
     ss.str("");
     ss << "UPLOAD_LOCATION=" << location.uploadLocation;
     env[2] = strdup(ss.str().c_str());
-
-	ss.str("");
-	ss << "REQUEST_METHOD=" << req.getMethod();
-	env[3] = strdup(ss.str().c_str());
-
+    ss.str("");
+    ss << "REQUEST_METHOD=" << req.getMethod();
+    env[3] = strdup(ss.str().c_str());
     ss.str("");
     ss << "QUERY_STRING=" << req.getBody() << CRLF;
     env[4] = strdup(ss.str().c_str());
-
     env[5] = NULL;
-
-    for (int j = 0; env[j]; j++) {
-        std::cout << "env[" << j << "] : " << env[j] << std::endl;
-    }
-
     return env;
 }
 
 void handleCGI(Configuration &Config, LocationBlock &location, Request &req, Response &res)
 {
-    // printCgiParams(location.cgiParams);
     char **env = createEnv(req, location);
     std::string cgiPathWithArgs = "./www/cgi-bin/test.py";
     std::stringstream cgiOutput;
     std::cout << "CGI PATH: " << cgiPathWithArgs << std::endl;
-
     int pipefd[2];
     if (pipe(pipefd) == -1)
     {
@@ -70,9 +35,7 @@ void handleCGI(Configuration &Config, LocationBlock &location, Request &req, Res
         res.setStatusCode(500);
         return;
     }
-
     int pid = fork();
-
     if (pid == -1)
     {
         std::cerr << "fork failed" << std::endl;
@@ -81,20 +44,17 @@ void handleCGI(Configuration &Config, LocationBlock &location, Request &req, Res
     }
     else if (pid == 0)
     {
-        // Child process
-        close(pipefd[0]); // Close unused read end
-        dup2(pipefd[1], STDOUT_FILENO); // Redirect stdout to pipe
-        dup2(pipefd[1], STDERR_FILENO); // Redirect stderr to pipe
-        close(pipefd[1]); // Close write end after duplicating
-
+        close(pipefd[0]);
+        dup2(pipefd[1], STDOUT_FILENO);
+        dup2(pipefd[1], STDERR_FILENO);
+        close(pipefd[1]);
         char *args[] = {strdup(cgiPathWithArgs.c_str()), NULL};
         execve(cgiPathWithArgs.c_str(), args, env);
-        exit(1); // execve failed
+        exit(1);
     }
     else
     {
-        // Parent process
-        close(pipefd[1]); // Close unused write end
+        close(pipefd[1]);
         char buffer[128];
         ssize_t bytesRead;
         while ((bytesRead = read(pipefd[0], buffer, sizeof(buffer) - 1)) > 0)
@@ -102,7 +62,7 @@ void handleCGI(Configuration &Config, LocationBlock &location, Request &req, Res
             buffer[bytesRead] = '\0';
             cgiOutput << buffer;
         }
-        close(pipefd[0]); // Close read end
+        close(pipefd[0]);
 
         int status;
         waitpid(pid, &status, 0);
@@ -110,8 +70,13 @@ void handleCGI(Configuration &Config, LocationBlock &location, Request &req, Res
         {
             std::cout << "child exited with status: " << WEXITSTATUS(status) << std::endl;
         }
-        res.setBody(cgiOutput.str()); // create a parsing function to parse the output and set correctly body and headers
+        res.setBody(cgiOutput.str());
         res.setMimeType("html");
         res.setStatusCode(200);
     }
+    for (int i = 0; env[i]; i++)
+    {
+        free(env[i]);
+    }
+    delete[] env;
 }
